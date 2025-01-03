19e7354f9e8a2f6ca0fc175353d06ab566be72fc
dlyman <danlyman.3dartist@gmail.com>
2025-01-02 17:55:07 -0700

    Update turretdeef.js

--- turretdeef.js
+++ turretdeef.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
 canvas.width = 800;
-canvas.height = 600;
+canvas.height = 400;
 
 // Fill the canvas with a black background
 ctx.fillStyle = "black";
